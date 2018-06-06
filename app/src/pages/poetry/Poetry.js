b566d4d147f7e2aba62400c8a67a310923b4f976
duwendi <987638631@qq.dom>
2018-06-07 00:05:48 +0800

    诗词时间轴首页

--- app/src/pages/poetry/Poetry.js
+++ app/src/pages/poetry/Poetry.js
@@ -3,37 +3,124 @@
  */
 import React, { Component } from 'react';
 import {
+    AppRegistry,
     StyleSheet,
-    View,
     Text,
-    Image,
+    View,
+    FlatList,
+    Dimensions,
+    Image
 } from 'react-native';
-import LinearGradient from 'react-native-linear-gradient';
-
-type Props = {};
-export default class Poetry extends Component<Props> {
-
-    constructor(props){
+var {width,height} = Dimensions.get('window');
+var dataAry = [];
+import data from './data.json';
+export default class Poetry extends Component {
+    // 构造
+    constructor(props) {
         super(props);
+        // 初始状态
         this.state = {
-
-        }
+            dataAry: dataAry,
+        };
     }
 
     render() {
-        let colors = ['#e2dbcb', '#dbd4c4', '#e5ddce'];
         return (
-            <Image style={styles.wrap} source= {require('../../images/poetry-bg-2.jpg')}>
-
-            </Image>
+            <View style={{marginTop:30}}>
+                <FlatList
+                    data = {this.state.dataAry}
+                    renderItem = {(item) => this.renderRow(item)}
+                    keyExtractor={this.keyExtractor}
+                />
+                <View style={{width:1,height:height,backgroundColor:'red',position:'absolute',left:50}}></View>
+            </View>
         );
     }
+
+    renderRow =(item) =>{
+        if(item.item.text){
+            return(
+                <View style={{marginBottom:10,marginLeft:60}}>
+                    <Text>{item.item.text}</Text>
+                </View>
+            )
+        }else{
+            return(
+                <View style={{flexDirection:'row',marginBottom:10}}>
+                    {/*左边*/}
+                    <View style={{width:60,marginBottom:10}}>
+                        <View style={{flexDirection:'row',alignItems:'center'}}>
+                            <Text>{item.item.time}</Text>
+                            <View style={{width:10,height:10,borderRadius:5,backgroundColor:'red',position:'absolute',left:45}}></View>
+                        </View>
+                    </View>
+                    {/*右边*/}
+                    <View style={{backgroundColor:"#F2F2F2",marginLeft:5,width:width-70}} onLayout = {(event)=>this.onLayout(event)} >
+                        <Text style={{}}>{item.item.content}</Text>
+                        {/*<View style={{flexDirection:'row',flexWrap:'wrap'}}>*/}
+                            {/*{this.renderImg(item.item.text)}*/}
+                        {/*</View>*/}
+                    </View>
+                </View>
+            )
+
+        }
+
+
+
+    }
+
+    keyExtractor(item: Object, index: number) {
+        return item.id
+    }
+
+    onLayout = (event)=>{
+        console.log(event.nativeEvent.layout.height)
+    }
+
+    renderImg = (imgAry) =>{
+        var renderAry = []
+        for(var i = 0;i < imgAry.length; i++){
+            if(imgAry.length == 1){
+                renderAry.push(
+                    <Image key={i} source={{uri:imgAry[0].url}} style={{width:200,height:200}}/>
+                )
+            }else if(imgAry.length == 2 || imgAry.length == 4){
+                renderAry.push(
+                    <Image key={i} source={{uri:imgAry[i].url}} style={{width:(width-70)*0.5-2,height:(width-70)*0.5-2,marginLeft:1,marginTop:1}}/>
+                )
+            }else {
+                renderAry.push(
+                    <Image key={i} source={{uri:imgAry[i].url}} style={{width:(width-70)/3-2,height:(width-70)/3-2,marginLeft:1,marginTop:1}}/>
+                )
+            }
+        }
+
+        return renderAry
+    }
+
+    componentDidMount() {
+        this.setState({
+            dataAry:data
+        })
+    }
 }
+
 const styles = StyleSheet.create({
-    wrap: {
-        flex:1,
-        alignItems: 'center',
+    container: {
+        flex: 1,
         justifyContent: 'center',
-        width: null,
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
     },
 });
