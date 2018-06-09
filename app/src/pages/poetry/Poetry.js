579bca0ca246cb51d4014462c300f599e6d60b79
duwendi <987638631@qq.dom>
2018-06-09 16:40:03 +0800

    Revert "修改样式"
    This reverts commit e379b9e15362980824f3d0520b61860ac2291747.

--- app/src/pages/poetry/Poetry.js
+++ app/src/pages/poetry/Poetry.js
@@ -3,11 +3,13 @@
  */
 import React, { Component } from 'react';
 import {
+    AppRegistry,
     StyleSheet,
     Text,
     View,
     FlatList,
-    Dimensions
+    Dimensions,
+    Image
 } from 'react-native';
 var {width,height} = Dimensions.get('window');
 var dataAry = [];
@@ -24,13 +26,13 @@ export default class Poetry extends Component {
 
     render() {
         return (
-            <View style={{marginTop:5}}>
+            <View style={{marginTop:30}}>
                 <FlatList
                     data = {this.state.dataAry}
                     renderItem = {(item) => this.renderRow(item)}
                     keyExtractor={this.keyExtractor}
                 />
-                <View style={{width:1,height:height,backgroundColor:'#50b6ff',position:'absolute',left:50}}></View>
+                <View style={{width:1,height:height,backgroundColor:'red',position:'absolute',left:50}}></View>
             </View>
         );
     }
@@ -49,12 +51,15 @@ export default class Poetry extends Component {
                     <View style={{width:60,marginBottom:10}}>
                         <View style={{flexDirection:'row',alignItems:'center'}}>
                             <Text>{item.item.time}</Text>
-                            <View style={styles.timeDot}></View>
+                            <View style={{width:10,height:10,borderRadius:5,backgroundColor:'red',position:'absolute',left:45}}></View>
                         </View>
                     </View>
                     {/*右边*/}
-                    <View style={styles.poemBox} onLayout = {(event)=>this.onLayout(event)} >
+                    <View style={{backgroundColor:"#F2F2F2",marginLeft:5,width:width-70}} onLayout = {(event)=>this.onLayout(event)} >
                         <Text style={{}}>{item.item.content}</Text>
+                        {/*<View style={{flexDirection:'row',flexWrap:'wrap'}}>*/}
+                            {/*{this.renderImg(item.item.text)}*/}
+                        {/*</View>*/}
                     </View>
                 </View>
             )
@@ -73,6 +78,27 @@ export default class Poetry extends Component {
         console.log(event.nativeEvent.layout.height)
     }
 
+    renderImg = (imgAry) =>{
+        var renderAry = []
+        for(var i = 0;i < imgAry.length; i++){
+            if(imgAry.length == 1){
+                renderAry.push(
+                    <Image key={i} source={{uri:imgAry[0].url}} style={{width:200,height:200}}/>
+                )
+            }else if(imgAry.length == 2 || imgAry.length == 4){
+                renderAry.push(
+                    <Image key={i} source={{uri:imgAry[i].url}} style={{width:(width-70)*0.5-2,height:(width-70)*0.5-2,marginLeft:1,marginTop:1}}/>
+                )
+            }else {
+                renderAry.push(
+                    <Image key={i} source={{uri:imgAry[i].url}} style={{width:(width-70)/3-2,height:(width-70)/3-2,marginLeft:1,marginTop:1}}/>
+                )
+            }
+        }
+
+        return renderAry
+    }
+
     componentDidMount() {
         this.setState({
             dataAry:data
@@ -81,20 +107,20 @@ export default class Poetry extends Component {
 }
 
 const styles = StyleSheet.create({
-    dynasty:{
-
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
     },
-    timeDot:{
-        width:10,
-        height:10,
-        borderRadius:5,
-        backgroundColor:'#50b6ff',
-        position:'absolute',
-        left:45
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
     },
-    poemBox: {
-        backgroundColor:"#fff0bd",
-        marginLeft:5,
-        width:width-70
-    }
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
